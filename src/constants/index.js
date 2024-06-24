6a27e4d7bc2696e3bc0a760781e84a5983ada6a1
Jaydeep Shrikrishna Rathod <jaydeep.rathod.2019it@gmail.com>
2024-06-24 22:41:53 +0530

    updated the description and personal info

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,17 +3,24 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `Welcome to my portfolio! I am a seasoned Frontend React Developer with over 2 years of experience in creating dynamic, user-centric web applications. My expertise lies in leveraging cutting-edge technologies and frameworks to deliver seamless, responsive, and visually appealing user interfaces. `;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `Here’s a glimpse into my skill set and the tools I master:
+
+React.js: Mastery in building robust, high-performance applications with React, ensuring maintainability and scalability.
+Ant Design (AntD): Skilled in implementing Ant Design components for sophisticated, enterprise-grade UI designs.
+Styled Components (SHDCN): Proficient in using styled components to write modular, maintainable CSS directly within JavaScript.
+Tailwind CSS: Expertise in rapidly creating custom designs with Tailwind CSS, achieving a perfect blend of flexibility and efficiency.
+Material-UI: Experienced in deploying Material-UI to craft intuitive, responsive, and visually consistent user interfaces.
+Deployment: Comprehensive knowledge of modern deployment strategies to ensure smooth, reliable application delivery and updates.`;
 
 export const EXPERIENCES = [
   {
     year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    role: "Frontend React Developer",
+    company: "Nivzen Technologies",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+    technologies: ["Javascript", "React.js", "ShadCN", "ANTD"],
   },
   {
     year: "2022 - 2023",
